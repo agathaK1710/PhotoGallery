53498d2422eeb518d4b2d36f38ed4f2d6e96818e
Agatha <agatha.kazak@gmail.com>
2021-09-21 09:57:38 +0300

    change request to GET RECENT INTERESTING PHOTOS

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,6 +4,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=f55eeb9feaf1a08a3b08d32860f1feb8" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
