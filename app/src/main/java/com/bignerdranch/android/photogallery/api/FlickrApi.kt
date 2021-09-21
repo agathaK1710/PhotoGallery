dc40c1216b1648574617f3c240e91bb9576d3796
Agatha <agatha.kazak@gmail.com>
2021-09-21 12:39:30 +0300

    feat: part 24

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.photogallery.api
+
+import com.bignerdranch.android.photogallery.FlickrResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=f55eeb9feaf1a08a3b08d32860f1feb8" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<FlickrResponse>
+}
